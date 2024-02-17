90774ec7dab9e55350d69e1591d1de92c74b5b37
Alexey Kudryavtsev <westor@me.com>
2024-02-17 19:43:10 +0800

    Add custom configuration to fork

---
+++ config.ini.php
@@ -0,0 +1,14 @@
+[system]
+
+enabled_bridges[] = Telegram
+enabled_bridges[] = Instagram
+enabled_bridges[] = YouTube
+enabled_bridges[] = Twitter
+enabled_bridges[] = Facebook
+enabled_bridges[] = TheHackerNews
+enabled_bridges[] = TldrTech
+
+[cache]
+
+; Cache type: file, sqlite, memcached, array, null
+type = "file"
